26b32a06b1bf055e9eb907a8b4f97718757a3366
lixiong <346959251@qq.com>
2016-10-16 14:07:53 +0800

    update

--- src/utils/common.js
+++ src/utils/common.js
@@ -51,7 +51,8 @@ class Common {
 	}
 	getApiInfo(){	// api相关配置
 		return {
-			apiPath : '/api/'	// 需要根据实际情况配置
+			// apiPath : '/api/'	// 生产环境配置
+			apiPath : '/dist/api/'	// 开发环境配置
 		}
 	}
 	isCorrectRender(response){	// 判断fetch返回结果是否正确
